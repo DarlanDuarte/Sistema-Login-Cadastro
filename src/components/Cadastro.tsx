830045cebabe8571a86034b24fd14112f828b271
DarlanDuarte <darlan.duarte.m@gmail.com>
2023-08-28 16:06:49 -0300

    Iniciando Projeto

---
+++ src/components/Cadastro.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Cadastro: React.FC = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cadastro;
---
+++ src/components/Login.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <div>Login</div>
+    </div>
+  );
+};
+
+export default Login;
